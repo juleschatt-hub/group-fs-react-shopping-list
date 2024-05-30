6df2a1543d50a50d3102d64f82dd5821c6f07aa3
Julian Chatterton <julian.james.chatterton@gmail.com>
2024-05-30 16:19:29 -0500

    copy pasted files worked on in group-work. GET and POST working

--- database.sql
+++ database.sql
@@ -1,2 +1,14 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+
+CREATE TABLE "shopping_list" (
+"id" SERIAL PRIMARY KEY,
+"name" varchar (80),
+"quantity" decimal,
+"unit" varchar (20),
+"purchased" boolean
+);
+
+INSERT INTO "shopping_list" ("name", "quantity", "unit", "purchased")
+VALUES ('apples', 5.5, 'lbs', false), ('bread', 1, 'loaf', false), ('milk', 1, 'gallon', false);
---
+++ server/routes/list.router.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool.js');
+
+router.get('/', (req, res) => {
+const sqlText = `SELECT * FROM "shopping_list";`;
+pool.query(sqlText)
+    .then((result) => {
+        console.log(`Got stuff back from the database`, result.rows);
+            res.send(result.rows);
+    })
+    .catch((error) => {
+        console.log(`Error making database query ${sqlText}`, error);
+        res.sendStatus(500);
+    })
+});
+
+router.post('/', (req, res)=> {
+    const item = req.body;
+    const sqlText = `INSERT INTO shopping_list (name, quantity, unit, purchased)
+    VALUES ($1, $2, $3, $4)`;
+    pool.query(sqlText, [item.name, item.quantity, item.unit, item.purchased])
+    .then((result) => {
+        console.log(`Added item to the database`, item);
+        res.sendStatus(201);
+    })
+    .catch((error) => {
+        console.log(`Error making database query ${sqlText}`, error);
+        res.sendStatus(500);
+    })
+});
+
+module.exports = router;
